3b441a76c9caa20ce24e15ff546574de1fdaf843
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-02-19 18:07:19 -0600

    map matrix

--- main.py
+++ main.py
@@ -1,20 +1,77 @@
 import pygame
 import random
 from data.sprites.player import Player
+#define screen sizes
+tile_size = 50
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+# create a screen object
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+        #load images
+        dirt_img = pygame.image.load('img/grass.jfif')
+
+        row_count = 0
+        for row in data: 
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                # if tile == 2:
+                #     img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                #     img_rect = img.get_rect()
+                #     img_rect.x = col_count * tile_size
+                #     img_rect.y = row_count * tile_size
+                #     tile = (img, img_rect)
+                #     self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+
+# map grid working 
+# Change 1's to something else in order to change map view
+# Then update the world class
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] 
+]
+
+world = World(world_data)
+
 
 def main():
     # initialize pygame
 
     pygame.init()
 
-    #define screen width and screen height
-
-    SCREEN_WIDTH = 800
-    SCREEN_HEIGHT = 600
-
 
     #create a player object
     player = Player()
+    world = World(world_data)
 
     # variable to keep game running
     running = True
@@ -29,13 +86,16 @@ def main():
         K_ESCAPE,
         KEYDOWN,
         QUIT,
-    )
+    ) 
 
-    # create a screen object
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))   
+    def draw_grid():
+        for line in range(0, 20):
+            pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (SCREEN_WIDTH, line * tile_size))
+            pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, SCREEN_HEIGHT))
     # Main Loop
     while running:
         #look at event queue
+
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
@@ -49,9 +109,12 @@ def main():
 
         screen.fill((200,0,0))
 
+        world.draw()
         screen.blit(player.surf, player.rect)
-
+        draw_grid()
         pygame.display.flip()
+
+        #pygame.display.update()
         
     
 if __name__ == '__main__': main()
